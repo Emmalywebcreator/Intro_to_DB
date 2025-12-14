813cc16a1f8f07ac899ddfd21d7d13c4e3cac0f6
Emmanuel Ohwoka <emmyprime2025@gmail.com>
2025-12-14 16:49:13 +0100

    A script that add into a table name Customers

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,45 +1,43 @@
-CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
---Create Authors table first
-CREATE TABLE Authors (
-	author_id INT PRIMARY KEY,
-	author_name VARCHAR(225)
+-- Create Authors table
+CREATE TABLE IF NOT EXISTS authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
 );
 
---Create Books tables (Depends on Authors)
-CREATE TABLE Books (
-	book_id INT PRIMARY KEY,
-	title VARCHAR(130),
-	author_id INT,
-	price DOUBLE,
-	publicaation_date DATE,
-	FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+-- Create Books table
+CREATE TABLE IF NOT EXISTS books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
 );
 
---Create Customers table (No dependencies)
-CREATE TABLE Customers (
-	customer_id INT PRIMARY KEY,
-	customer_name VARCHAR(215),
-	email VARCHAR(215),
-	address TEXT
+-- Create Customers table (singular: customer)
+CREATE TABLE IF NOT EXISTS customer (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
 );
 
---Create Orders table (depends on Customers table)
-CREATE TABLE Orders (
-	order_id INT PRIMARY KEY,
-	customer_id INT,
-	order_date DATE,
-	FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+-- Create Orders table
+CREATE TABLE IF NOT EXISTS orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
 );
 
---Create Order Details table (This will depends on the Order and Books table)
-CREATE TABLE Order_Details (
-        order_detailid INT PRIMARY KEY,
-	order_id INT,
-	book_id INT,
-	quantity DOUBLE,
-	FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-	FOREIGN KEY (book_id) REFERENCES Books(book_id)
+-- Create Order_Details table
+CREATE TABLE IF NOT EXISTS order_details (
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id)
 );
-
