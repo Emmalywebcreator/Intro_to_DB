d43252a3e812326a13c61ae72ee4a6866feea8b7
Emmanuel Ohwoka <emmyprime2025@gmail.com>
2025-12-14 11:25:54 +0100

    updated the alx_book_store.sql file to fix a bug

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -40,6 +40,6 @@ CREATE TABLE Order_Details (
 	book_id INT,
 	quantity DOUBLE,
 	FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-	FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+	FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
 
